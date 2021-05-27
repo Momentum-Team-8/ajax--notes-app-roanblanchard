0516a775a899827c4dd6d1f5763327cd4b0b13d6
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-27 13:04:52 -0400

    added button that grabs the note and displays it

---
+++ script.js
@@ -0,0 +1,31 @@
+const url = 'http://localhost:3000/notes/'
+const output = document.querySelector('.output')
+const getNotesButton = document.querySelector('.button')
+
+function getListOfNotes () {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        
+        for (let x of data) {
+        // generates the title of the note
+        const title = document.createElement('p')
+        title.innerHTML = "Title: " + data[0].title
+        output.appendChild(title)
+
+        // generates the body of the note
+        const body = document.createElement('p')
+        body.innerHTML = "Body: " + data[0].body
+        output.appendChild(body)
+        }
+    })
+}
+
+
+
+document.getElementById("button").addEventListener("click", event => {
+    event.preventDefault()
+    getListOfNotes()
+})
+
+// getListOfNotes()
