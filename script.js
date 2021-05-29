3838c90e66c753b5f7b181fd1684059a332c67d6
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-29 14:01:23 -0400

    submit form clears when button is pressed

--- script.js
+++ script.js
@@ -3,51 +3,17 @@ const form = document.querySelector('#note-form')
 const output = document.querySelector('.notes')
 const getNotesButton = document.querySelector('.button')
 
+
+// iterates through notes and renders them to the screen
 function getListOfNotes () {
     fetch(url)
     .then(response => response.json())
     .then(data => {
         
         for (let x of data) {
-        // // generates the title of the note
-        // const title = document.createElement('p')
-        // title.innerHTML = "Title: " + x.title
-        // output.appendChild(title)
-
-        // // generates the body of the note
-        // const body = document.createElement('p')
-        // body.innerHTML = "Body: " + x.body
-        // output.appendChild(body)
-
         renderNotes(x)
         }
     })
-}
-
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    const noteText = document.getElementById('newNote').value
-    createNote(noteText)
-})
-
-function createNote(noteText) {
-    // I am making a POST request so that I can add
-    // a new todo to my database. 
-    fetch(url, {
-        // I need to send some extra information with this request
-        // specifically i am sending the value of my input on the DOM
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            title: noteText,
-            body: noteText,
-            // here I am creating a new key and using moment().format()
-            // to create a time string that captures when the new todo was created
-            create_at: moment().format()
-        })
-    })
-    .then(response => response.json())
-    .then(data => renderNotes(data))
 }
 
 function renderNotes(noteObj) {
@@ -61,17 +27,40 @@ function renderNotes(noteObj) {
     output.appendChild(itemEl)
 }
 
-   // This function is taking two arguments: a todo <li> a todo object.
 function renderNoteText(noteListItem, noteObj) {
-       // Here I am setting the content of my <li> to the body of my todo object
     noteListItem.innerHTML = noteObj.body
     }
 
+// end of note rendering
 
 
-// document.getElementById("button").addEventListener("click", event => {
-//     event.preventDefault()
-//     getListOfNotes()
-// })
+// listens for the submit button to be pressed, submits a new note
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const noteText = document.getElementById('newNote').value
+    createNote(noteText)
+    location.reload();
+})
 
+
+// handles creating a new note using a post request
+function createNote(noteText) {
+    fetch(url, {
+
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            title: noteText,
+            body: noteText,
+            create_at: moment().format()
+        })
+    })
+    .then(response => response.json())
+    .then(data => renderNotes(data))
+}
+
+
+
+
+// calls function to display notes
 getListOfNotes()
